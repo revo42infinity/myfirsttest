34b2ab94d853fe051c8350c2f3865a7f4eb99f1e
owner <canerduru@ymail.com>
2020-12-19 17:48:54 +0300

    Initial Commit

---
+++ myfirsttest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myfirsttest
+//
+//  Created by owner on 12/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
